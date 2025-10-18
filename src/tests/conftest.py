ff25d0cb3c94004ea73de16a092323695ed46633
Masoud Salehi <m.salehi7412@gmail.com>
2025-10-18 17:14:17 +0200

    CI: add repo root to sys.path and PYTHONPATH so run_pipeline imports

--- src/tests/conftest.py
+++ src/tests/conftest.py
@@ -3,8 +3,11 @@ import os, sys
 
 # HERE = .../src/tests
 HERE = os.path.dirname(__file__)
-# SRC = .../src
-SRC = os.path.abspath(os.path.join(HERE, ".."))
+# SRC  = .../src
+SRC  = os.path.abspath(os.path.join(HERE, ".."))
+# ROOT = project root (parent of src)
+ROOT = os.path.abspath(os.path.join(SRC, ".."))
 
-if SRC not in sys.path:
-    sys.path.insert(0, SRC)
+for p in (SRC, ROOT):
+    if p not in sys.path:
+        sys.path.insert(0, p)
